9608aa97ef3021f5ce09ad0de9fd97d17a205787
mintchoc31 <mintchoc31@gmail.com>
2023-08-31 19:11:27 +0900

    Jboard2 기능 추가

--- Jboard2/src/main/java/kr/co/jboard2/service/ArticleService.java
+++ Jboard2/src/main/java/kr/co/jboard2/service/ArticleService.java
@@ -28,20 +28,49 @@ public enum ArticleService {
 		return dao.insertArticle(dto);
 	}
 	
-	public ArticleDTO selectArticle(int no) {
+	public ArticleDTO selectArticle(String no) {
 		return dao.selectArticle(no);
 	}
 	
-	public List<ArticleDTO> selectArticles() {
-		return dao.selectArticles();
+	public List<ArticleDTO> selectArticles(int start) {
+		return dao.selectArticles(start);
 	}
 	
 	public void updateArticle(ArticleDTO dto) {
 		dao.updateArticle(dto);
 	}
 	
-	public void deleteArticle(int no) {
+	public void deleteArticle(String no) {
 		dao.deleteArticle(no);
+	}
+	
+	// 추가
+	public int selectCountTotal() {
+		return dao.selectCountTotal();
+	}
+	
+	public List<ArticleDTO> selectComments(String parent) {
+		return dao.selectComments(parent);
+	}
+	
+	public void insertComment(ArticleDTO dto) {
+		dao.insertComment(dto);
+	}
+	
+	public void updateArticleForCommentPlus(String no) {
+		dao.updateArticleForCommentPlus(no);
+	}
+	
+	public void updateArticleForCommentMinus(String no) {
+		dao.updateArticleForCommentMinus(no);
+	}
+	
+	public void updateComment(String no, String content) {
+		dao.updateComment(no, content);
+	}
+	
+	public void deleteComment(String no) {
+		dao.deleteComment(no);
 	}
 	
 	// 업로드 경로 구하기
@@ -101,5 +130,55 @@ public enum ArticleService {
 	public void downloadFile() {
 		
 	}
-	
+	// 페이지 마지막 번호
+		public int getLastPageNum(int total) {
+			
+			int lastPageNum = 0;
+			
+			if(total % 10 == 0){
+				lastPageNum = total / 10;
+			}else{
+				lastPageNum = total / 10 + 1;
+			}
+			
+			return lastPageNum;
+		}
+		
+		// 페이지 그룹
+		public int[] getPageGroupNum(int currentPage, int lastPageNum) {
+			int currentPageGroup = (int)Math.ceil(currentPage / 10.0);
+			int pageGroupStart = (currentPageGroup - 1) * 10 + 1;
+			int pageGroupEnd = currentPageGroup * 10;
+			
+			if(pageGroupEnd > lastPageNum){
+				pageGroupEnd = lastPageNum;
+			}
+			
+			int[] result = {pageGroupStart, pageGroupEnd};
+			
+			return result;
+		}
+		
+		// 페이지 시작번호
+		public int getPageStartNum(int total, int currentPage) {
+			int start = (currentPage - 1) * 10;
+			return total - start;
+		}
+		
+		// 현재 페이지 번호
+		public int getCurrentPage(String pg) {
+			int currentPage = 1;
+			
+			if(pg != null){
+				currentPage = Integer.parseInt(pg);	
+			}
+			
+			return currentPage;
+		}
+		
+		// Limit 시작번호
+		public int getStartNum(int currentPage) {
+			return (currentPage - 1) * 10;
+		}
+		
 }
